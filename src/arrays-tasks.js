eddeb04c7390422cdecf8e1372206ca5ba4e94dd
Yorokay <yorokay05110@mail.ru>
2024-01-04 05:09:06 +0300

    feat: solve first task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  let arr = new Array(end - start + 1)
+    .fill(null)
+    .map((item, index) => index + start);
+  arr = Array.from(arr);
+  return arr;
 }
 
 /**
---
+++ test.js
@@ -0,0 +1,17 @@
+// function getIntervalArray(start, end) {
+//   let arr = new Array(end - start + 1).fill(null);
+//   let arr = new Array(end - start + 1).fill(null);
+//   arr.length = end - start + 1;
+
+//   arr = arr
+//   .fill(null)
+//   .map((item, index) => index + start);
+
+//   return arr;
+// }
+
+// console.log(getIntervalArray(1, 5))
+
+// let lol = [];
+// lol.length = 5;
+// console.log(lol.length)
